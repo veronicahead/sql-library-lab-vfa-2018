8853c0b2fe18bd3fb9cb4a86ebf95b779c8a5f4e
Veronica Head <veronica.leigh.head@gmail.com>
2018-06-25 21:13:27 +0000

    automatically backed up by learn

--- lib/querying.rb
+++ lib/querying.rb
@@ -30,5 +30,9 @@ def select_series_title_with_most_human_characters
 end
 
 def select_character_names_and_number_of_books_they_are_in
-  ""
+  "SELECT characters.name, COUNT(*) AS book_count
+  FROM character_books
+  JOIN characters ON character_books.character_id = characters.id
+  GROUP BY characters.name
+  ORDER BY book_count DESC"
 end
