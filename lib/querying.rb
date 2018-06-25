3602145edf50e848c215edcd540bd7d7c151ae09
Veronica Head <veronica.leigh.head@gmail.com>
2018-06-25 20:50:02 +0000

    automatically backed up by learn

--- lib/querying.rb
+++ lib/querying.rb
@@ -1,5 +1,5 @@
 def select_books_titles_and_years_in_first_series_order_by_year
-  "SELECT books, titles, years FROM "
+  "SELECT title, year FROM books ORDER BY year WHERE series_id = 1;"
 end
 
 def select_name_and_motto_of_char_with_longest_motto
