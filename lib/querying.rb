623c1610282b754160a25f827900ed1a5a394a22
Veronica Head <veronica.leigh.head@gmail.com>
2018-06-25 21:10:30 +0000

    automatically backed up by learn

--- lib/querying.rb
+++ lib/querying.rb
@@ -22,7 +22,7 @@ def select_series_title_with_most_human_characters
   "SELECT series.title
   FROM series
   JOIN books ON books.series_id = series.id
-  JOIN character_books ON character_books.id = books.id
+  JOIN character_books ON character_books.book_id = books.id
   JOIN characters ON character_books.character_id = characters.id
   WHERE characters.species = 'human'
   GROUP BY series.title
