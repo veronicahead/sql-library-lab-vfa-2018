d2e989a0f7e84f4f4cc1e0367b77b4cc6c54fa0e
Veronica Head <veronica.leigh.head@gmail.com>
2018-06-25 21:07:31 +0000

    automatically backed up by learn

--- lib/querying.rb
+++ lib/querying.rb
@@ -23,7 +23,7 @@ def select_series_title_with_most_human_characters
   FROM series
   JOIN books ON books.series_id = series.id
   JOIN character_books ON character_books.id = books.id
-  JOIN characters ON "
+  JOIN characters ON characters.id = character_books.character_id"
 end
 
 def select_character_names_and_number_of_books_they_are_in
