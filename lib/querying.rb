69fc458fe8492365abd2301fa309ecffdcfb206b
Veronica Head <veronica.leigh.head@gmail.com>
2018-06-25 20:56:00 +0000

    automatically backed up by learn

--- lib/querying.rb
+++ lib/querying.rb
@@ -8,11 +8,11 @@ end
 
 
 def select_value_and_count_of_most_prolific_species
-  "SELECT species, COUNT(*) FROM characters GROUP BY species "
+  "SELECT species, COUNT(*) FROM characters GROUP BY species ORDER BY COUNT(species) DESC LIMIT 1"
 end
 
 def select_name_and_series_subgenres_of_authors
-  "Write your SQL query here"
+  "SELECT authors.name, "
 end
 
 def select_series_title_with_most_human_characters
