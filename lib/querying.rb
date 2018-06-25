633d5b6765e34285b6068a318adc78ec74d35404
Veronica Head <veronica.leigh.head@gmail.com>
2018-06-25 20:59:13 +0000

    automatically backed up by learn

--- lib/querying.rb
+++ lib/querying.rb
@@ -14,7 +14,8 @@ end
 def select_name_and_series_subgenres_of_authors
   "SELECT authors.name, subgenres.name 
   FROM authors 
-  JOIN series ON series.author_id = authors.id"
+  JOIN series ON series.author_id = authors.id
+  JOIN series"
 end
 
 def select_series_title_with_most_human_characters
