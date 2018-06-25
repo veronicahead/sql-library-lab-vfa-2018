d86133fa4cf289f51d10442aa68be3139860142e
Veronica Head <veronica.leigh.head@gmail.com>
2018-06-25 20:54:48 +0000

    automatically backed up by learn

--- lib/querying.rb
+++ lib/querying.rb
@@ -8,7 +8,7 @@ end
 
 
 def select_value_and_count_of_most_prolific_species
-  "SELECT species, COUNT(species) FROM characters ORDER BY "
+  "SELECT species, COUNT(*) FROM characters ORDER BY "
 end
 
 def select_name_and_series_subgenres_of_authors
