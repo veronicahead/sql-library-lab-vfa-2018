47c1004036730e93092e6e14c9976d94b7bc5337
Veronica Head <veronica.leigh.head@gmail.com>
2018-06-25 20:42:57 +0000

    automatically backed up by learn

---
+++ lib/schema.sql
@@ -0,0 +1,38 @@
+CREATE TABLE series (
+  id INTEGER PRIMARY KEY,
+  title TEXT,
+  author INTEGER,
+  sub_genre INTEGER
+);
+
+CREATE TABLE sub_genre (
+  id INTEGER PRIMARY KEY,
+  name TEXT
+);
+
+CREATE TABLE authors (
+  id INTEGER PRIMARY KEY,
+  name TEXT
+);
+
+CREATE TABLE books (
+  id INTEGER PRIMARY KEY,
+  title TEXT,
+  year INTEGER,
+  series TEXT
+);
+
+CREATE TABLE characters (
+  id INTEGER PRIMARY KEY,
+  name TEXT,
+  motto TEXT,
+  species TEXT,
+  author TEXT,
+  series TEXT
+);
+
+CREATE TABLE character_books (
+  id INTEGER PRIMARY KEY,
+  character_id INTEGER FOREIGN KEY,
+  book_id INTEGER FOREIGN KEY
+);
