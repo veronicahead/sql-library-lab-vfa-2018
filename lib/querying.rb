9bb9b0a95586cf204630b5ba56de3f1b833316fd
Veronica Head <veronica.leigh.head@gmail.com>
2018-06-25 21:03:05 +0000

    automatically backed up by learn

--- lib/querying.rb
+++ lib/querying.rb
@@ -19,7 +19,9 @@ def select_name_and_series_subgenres_of_authors
 end
 
 def select_series_title_with_most_human_characters
-  ""
+  "SELECT series.title
+  FROM series
+  "
 end
 
 def select_character_names_and_number_of_books_they_are_in
